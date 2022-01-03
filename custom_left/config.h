a7688412752b718aff31f8a1ace622bfaed01a03
Morphy Kuffour <morphy.kuffour@uconn.edu>
2022-01-03 17:33:55 -0500

    fixing master and slave reelationship

--- custom_left/config.h
+++ custom_left/config.h
@@ -20,6 +20,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define USE_SERIAL
 
 #define MASTER_LEFT
-//#define MASTER_RIGHT
 
-//#define EE_HANDS
+#define SPLIT_USB_DETECT
+#define SPLIT_USB_TIMEOUT 2000
+
+#define EE_HANDS
